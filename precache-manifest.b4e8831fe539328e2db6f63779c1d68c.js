38b2b6a6938464d6907319db81f1434998e45581
Raja Singh <rajasingh9721@gmail.com>
2020-09-13 23:08:54 +0530

    Updates

---
+++ precache-manifest.b4e8831fe539328e2db6f63779c1d68c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e7ce716c90b7d49bef84b528b71998",
+    "url": "/netartassignment/index.html"
+  },
+  {
+    "revision": "e246c9ee9e9517eebeb0",
+    "url": "/netartassignment/static/css/main.80efac6f.chunk.css"
+  },
+  {
+    "revision": "bd244202ad6e47a3014f",
+    "url": "/netartassignment/static/js/2.dd53ef84.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/netartassignment/static/js/2.dd53ef84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e246c9ee9e9517eebeb0",
+    "url": "/netartassignment/static/js/main.01cdd50f.chunk.js"
+  },
+  {
+    "revision": "d846e1101be637ec4fb2",
+    "url": "/netartassignment/static/js/runtime-main.948af531.js"
+  },
+  {
+    "revision": "29d96123f6fe54ac99593cfc60bbef44",
+    "url": "/netartassignment/static/media/1.29d96123.png"
+  },
+  {
+    "revision": "57d5c6509602db21e03912c7a89a79e4",
+    "url": "/netartassignment/static/media/2.57d5c650.png"
+  },
+  {
+    "revision": "b5ddb1417b1786fc22028f4c49e4ffe3",
+    "url": "/netartassignment/static/media/3.b5ddb141.png"
+  },
+  {
+    "revision": "5762018e4279189fa56715c827648c1c",
+    "url": "/netartassignment/static/media/logo.5762018e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/netartassignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
